b59ec744c4d96aca049843afebb1ad6ec369823f
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-05-14 19:00:21 +0300

    Added reading of pokeefile from function reading ENV vars

--- main.go
+++ main.go
@@ -4,11 +4,22 @@ import (
     "fmt"
     "gopoke/poker"
     "time"
+    "os"
 )
+
+func pokeefilepath() (pokeefile string) {
+    if os.Getenv("GOPOKE_POKEES") == "" {
+        pokeefile = os.Getenv("HOME") + ".local/share/gopoke/pokees.json"
+    } else {
+        pokeefile = os.Getenv("GOPOKE_POKEES")
+    }
+
+    return
+}
 
 func main() {
     start := time.Now()
-	prs := poker.PokeAll("pokees.json")
+	prs := poker.PokeAll(pokeefilepath())
     diff := time.Now().Sub(start)
 
     fmt.Println("Site\t\t\tBytes read\tTime to read")
