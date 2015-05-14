f8717ee8ab41bbb01ab877c8a5a88aa90d5900f4
Rickard Andersson <rickard.m.andersson@gmail.com>
2015-05-14 18:06:15 +0300

    Added time keeping for main process to measure total time

--- main.go
+++ main.go
@@ -3,12 +3,18 @@ package main
 import (
     "fmt"
     "gopoke/poker"
+    "time"
 )
 
 func main() {
+    start := time.Now()
 	prs := poker.PokeAll("pokees.json")
+    diff := time.Now().Sub(start)
 
+    fmt.Println("Site\t\t\tBytes read\tTime to read")
+    fmt.Println("------------------------------------------------------------")
 	for _, pr := range prs {
 		fmt.Printf("%s\n", pr)
 	}
+    fmt.Printf("\nTotal time:\t\t%s\n", diff)
 }
